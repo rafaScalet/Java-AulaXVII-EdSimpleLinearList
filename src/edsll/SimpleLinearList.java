2ef42f6d577fcd2da588a2cd9b7199ffdc33fd98
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-18 18:06:58 -0300

    created the 'add' function

--- src/edsll/SimpleLinearList.java
+++ src/edsll/SimpleLinearList.java
@@ -4,12 +4,22 @@ public class SimpleLinearList {
     private Node top;
     private Node bottom;
 
-    public SimpleLinearList(){
+    public SimpleLinearList() {
         this.top = null;
         this.bottom = null;
     }
 
-    private boolean empty(){
+    private boolean empty() {
         return (this.top == null && this.bottom == null);
+    }
+
+    public void add(Node node) {
+        if (empty()) {
+            this.top = this.bottom = node;
+        } else {
+            Node serv = this.top;
+            this.top = node;
+            this.top.setNext(serv);
+        }
     }
 }
