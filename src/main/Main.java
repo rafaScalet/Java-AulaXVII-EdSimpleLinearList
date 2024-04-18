f5c70c6ff02e3984f61bd87b1d9a06b801e436db
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-18 18:22:26 -0300

    modified the 'main' class, program is finished UwU

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,24 @@
 package main;
 
+import java.util.Scanner;
+import edsll.Node;
+import edsll.SimpleLinearList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        SimpleLinearList list = new SimpleLinearList();
+        Scanner in = new Scanner(System.in);
+
+        for(int number = 0, indx = 0; number >=0; indx++){
+            System.out.print("Numbers: ");
+            number = in.nextInt();
+            if(number < 0){
+                break;
+            }
+            list.add(new Node(indx, number));
+            list.append(new Node(indx, number));
+        }
+        list.show();
+        in.close();
     }
 }
